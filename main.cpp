cc0cace15b72172ac6e0615cf3335f4120367f6a
MonsterMannen <viktorsvensson94@gmail.com>
2017-09-28 00:04:54 +0200

    bugfix. now know what images have message in them

--- main.cpp
+++ main.cpp
@@ -13,16 +13,27 @@ int changeLastChar(int, int);
 
 int main(int argc, char** argv) {
 	// ***args***
-	argc = 2;
-	argv[1] = "e_img1.png";
-	//argv[2] = "secret";
-	//argv[3] = "stuff";
-	//argv[4] = "hoho";
+	bool encrypt = false;	// debug
+
+	if (encrypt) {
+		// encrypt
+		argc = 7;
+		argv[1] = "img1.png";
+		argv[2] = "secret";
+		argv[3] = "stuff";
+		argv[4] = "vs1";
+		argv[5] = "derp";
+		argv[6] = "420";
+	}else {
+		// decrypt
+		argc = 2;
+		argv[1] = "e_img1.png";
+	}
 	// **********
 
 	// check arguments
 	if (argc == 1) {
-		cerr << "Usage: imagename.png hello this text will be encrypted\n";
+		cerr << "Usage: imagename.png textToEncrypt\n";
 		return -1;
 	}
 
@@ -43,7 +54,7 @@ int main(int argc, char** argv) {
 	if (!image.data) {
 		cerr << "Could not get image data\n";
 		return -1;
-	} else if (message.length() > (image.rows * image.cols)) {
+	} else if (message.length() + 3 > (image.rows * image.cols)) {
 		cerr << "Too small image for that message\n";
 		return -1;
 	}
@@ -53,7 +64,7 @@ int main(int argc, char** argv) {
 		clock_t begin = clock();
 		string text = readTextFromImage(image);
 		double decryptTime = double(clock() - begin);
-		cout << "\nDecrypted message (" << decryptTime << "ms): " << text << "\n";
+		cout << "\nDecrypted message (" << decryptTime << "ms): \n" << text << "\n";
 	} else {
 		// encrypt
 		clock_t begin = clock();
@@ -85,7 +96,7 @@ Mat writeTextToImage(Mat image, string message) {
 	int digit;
 	int row;
 	int col;
-	int count = 0;
+	int count = 3;	// skip 3 first pixels
 	for (char ch : message) {
 		row = count / image.cols;
 		col = count % image.cols;
@@ -106,6 +117,8 @@ Mat writeTextToImage(Mat image, string message) {
 		//cout << "to red: " << ascii % 10 << "\n";
 	}
 
+	//printPixels(image, 30);
+
 	return image;
 }
 
@@ -117,8 +130,24 @@ string readTextFromImage(Mat image) {
 	int ascii;
 	char ch;
 
+	//printPixels(image, 30);	// test
+
+	// check so that first 3 pixels end with 0
+	for (int testCol = 0; testCol < 3; testCol++) {
+		red = (image.at<Vec3b>(0, testCol)[2] + 1 - 1) % 10;
+		green = (image.at<Vec3b>(0, testCol)[1] + 1 - 1) % 10;
+		blue = (image.at<Vec3b>(0, testCol)[0] + 1 - 1) % 10;
+		if (red != 0 || green != 0 || blue != 0) {
+			cout << "\nNo encrypted message found\n";
+			return "";
+		}
+	}
+	
+
 	for (int r = 0; r < image.rows; r++) {
 		for (int c = 0; c < image.cols; c++) {
+			if (r == 0 && c < 3) continue;	// skip 3 first
+
 			red = (image.at<Vec3b>(r, c)[2] + 1 - 1) % 10;
 			green = (image.at<Vec3b>(r, c)[1] + 1 - 1) % 10;
 			blue = (image.at<Vec3b>(r, c)[0] + 1 - 1) % 10;
